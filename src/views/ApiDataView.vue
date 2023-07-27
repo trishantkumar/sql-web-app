cfeb90b891f18eb364cc9f36a3e4d1df54642da3
Trishant Kumar <trishant.k@simformsolutions.com>
2023-07-27 17:44:21 +0530

    Addedm button

--- src/views/ApiDataView.vue
+++ src/views/ApiDataView.vue
@@ -1,6 +1,14 @@
 <template>
     <div class="mt-4">
-        <div class="text-4xl font-bold text-center">Products Data from API</div>
+        <div class="flex mt-2 text-right">
+            <div class="w-4/5">
+                <p class="text-5xl font-bold">Products Data from API</p>
+            </div>
+            <div  class="w-2/5">
+                <button class="h-10 ml-4 bg-blue-600 mr-1 hover:bg-blue-700 text-white font-bold px-4 rounded" @click="$router.push({path: '/'})">Go
+                    to Home page</button>
+            </div>
+        </div>
         <Table :tableData="apiData" v-if="apiData && apiData.length" class="my-10" />
         <v-pagination v-model="page" :pages="10" :range-size="1" active-color="#DCEDFF" @update:modelValue="updateHandler"
             class="justify-center" />
